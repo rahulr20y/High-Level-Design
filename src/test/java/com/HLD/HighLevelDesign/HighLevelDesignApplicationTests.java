db8851f6f3ab303c8a6c8b43edd52bc46f2de265
rahulr20y <rahulr20y@gmail.com>
2024-01-20 12:11:38 +0530

    initialization

---
+++ src/test/java/com/HLD/HighLevelDesign/HighLevelDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HLD.HighLevelDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighLevelDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
